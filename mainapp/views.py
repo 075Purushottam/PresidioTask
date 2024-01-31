f45f6433d74e66c1ad7aacada3f997e82b363b61
Pintu <pintupatidar555@gmail.com>
2024-01-31 18:41:24 +0530

    update path problem

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 import pandas as pd
+import os
 
 def home(request):
-    excel_file_path = '..\EmployeeManagement\Employees.xlsx'
+    # excel_file_path = '..\EmployeeManagement\Employees.xlsx'
+    project_root = os.path.dirname(os.path.realpath(__file__))
 
+    excel_file_path = os.path.join(project_root, 'Employees.xlsx')
     df = pd.read_excel(excel_file_path)
     
     employees = df.to_dict(orient='records')
