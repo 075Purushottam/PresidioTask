c1dbecbf937e46304e3ef74492d28a6f4fe36276
Pintu <pintupatidar555@gmail.com>
2024-01-31 17:37:09 +0530

    add requirements.txt file

--- EmployeeManagement/settings.py
+++ EmployeeManagement/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(ux2aa161w739ja6v00c096z&1q*duz@2m8khku#+az#%e47b='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
